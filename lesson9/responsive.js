663e4b90b32465a1eaa16961eaaab8fda6cde841
xevious <xeviousgreen@gmail.com>
2020-03-23 23:56:54 -0600

    update

--- lesson9/responsive.js
+++ lesson9/responsive.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    var x = document.getElementById("mymenu");
+    if (x.className === "menu") {
       x.className += " responsive";
     } else {
-      x.className = "topnav";
+      x.className = "menu";
     }
   }
