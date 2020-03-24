5cfbdc024617f6bd941a015acd83a06fd8ada379
xevious <xeviousgreen@gmail.com>
2020-03-23 22:48:06 -0600

    updates

--- lesson-10/weekweather.js
+++ lesson-10/weekweather.js
@@ -12,11 +12,14 @@ const weekURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&uni
     for (i = 0; i <= 40; i++) {
         if (weekcast.list[i].dt_txt.includes("18:00:00")) {
 
+            var imagesrc = 'https://openweathermap.org/img/wn/' + weekcast.list[i].weather[0].icon + '@2x.png';
+            var desc = weekcast.list[i].weather[0].description;
 
             document.getElementById('day' + c).textContent = Math.round(weekcast.list[i].main.temp) +'F'; 
-
+            document.getElementById('days' + c).setAttribute('src', imagesrc);
+            document.getElementById('days' + c).setAttribute('alt', desc);
             c=c+1;
-        
+
 
 
 
