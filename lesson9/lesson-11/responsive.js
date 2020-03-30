395eb5b0024013762ca8c86314fc4b13d50d7de9
xevious <xeviousgreen@gmail.com>
2020-03-30 12:38:08 -0600

    update

--- lesson9/lesson-11/responsive.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("mymenu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
-}
--- lesson9/responsive.js
+++ lesson9/responsive.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("mymenu");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
+  var x = document.getElementById("mymenu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
   }
+}
